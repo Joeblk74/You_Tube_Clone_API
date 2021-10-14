8e6dc5ccf0180c606ad5b14134e8f1b0355fe7d7
joeblk74 <joeblk@gmail.com>
2021-10-14 12:03:04 -0800

    Update with CORS

--- you_tube_clone_API/you_tube_clone_API/settings.py
+++ you_tube_clone_API/you_tube_clone_API/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'comments'
+    'comments',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'you_tube_clone_API.urls'
@@ -125,3 +127,5 @@ try:
     from you_tube_clone_API.local_settings import *
 except ImportError:
     pass
+
+CORS_ORIGIN_ALLOW_ALL=True
